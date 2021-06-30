12727a323e18d5e0d26f9129491b347498c45137
Patricio23Cadena <66183223+Patricio23Cadena@users.noreply.github.com>
2021-06-30 16:47:07 -0500

    Add files via upload

---
+++ eduklita-ejb/ejbModule/com/entities/personas/Persona.java
@@ -0,0 +1,95 @@
+package com.entities.personas;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario",catalog="Eduklita_Relacional",schema="public")
+public class Persona implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_usuario")
+	private int id_usuario;
+	@Column(name="cdi")
+	private long cdi;
+	@Column(name="nombres")
+	private String nombres;
+	@Column(name="apellidos")
+	private String apellidos;
+	@Column(name="edad")
+	private int edad;
+	@Column(name="correo_electronico")
+	private String correo;
+	@Column(name="tipo")
+	private int tipo;
+	@Column(name="flag")
+	private int flag;
+	@Column(name="clave")
+	private String clave;
+	public long getCdi() {
+		return cdi;
+	}
+	public void setCdi(long cdi) {
+		this.cdi = cdi;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	public int getFlag() {
+		return flag;
+	}
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+}
