211703b1bcf0b17fdaa98f19b248de3174fe7358
Patricio23Cadena <66183223+Patricio23Cadena@users.noreply.github.com>
2021-07-10 18:33:11 -0500

    Add files via upload

---
+++ eduklita-ejb/ejbModule/com/entities/personas/Total.java
@@ -0,0 +1,66 @@
+package com.entities.personas;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({ @NamedQuery(name="Total.totalumnos", query = "SELECT t FROM Total t WHERE t.anio = 2017 ")})
+@Table(name="totalalumnos",catalog="Eduklita_Relacional",schema="public")
+public class Total implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int provincia;
+	@Column(name="anio")
+	private int anio;
+	@ManyToOne
+	@JoinColumn(name="provincia", referencedColumnName = "id_provincia",insertable = false,updatable = false)
+	private Provincia prov;
+	@Column(name="total")
+	private double total;
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public Provincia getProvincia() {
+		return prov;
+	}
+	public void setProvincia(Provincia prov) {
+		this.prov = prov;
+	}
+	
+	public int getId_provincia() {
+		return provincia;
+	}
+	public void setId_provincia(int id_provincia) {
+		this.provincia = id_provincia;
+	}
+	
+}
